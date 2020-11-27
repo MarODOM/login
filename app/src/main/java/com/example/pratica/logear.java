99ae3efe193b223b01e5f70a74029f63a9902145
MarODOM <marce.21.03.99@gmail.com>
2020-11-26 19:53:41 -0500

    version 1

---
+++ app/src/main/java/com/example/pratica/logear.java
@@ -0,0 +1,20 @@
+package com.example.pratica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class logear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logear);
+    }
+
+    public void contacto(View view) {
+    }
+}
